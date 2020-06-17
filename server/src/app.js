3beb8ab3b506b61caa84dd38abc88beba7161624
AbeerKaram <abeerkhalid571@gmail.com>
2020-06-17 15:15:38 +0300

    express boilerplate

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const compression = require('compression');
+const router = require('./router');
+
+const app = express();
+
+app.use(compression());
+app.disable('x-powerd-by');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.json());
+app.use('api/v1/', router);
+module.exports = app;
