4c693796b8ad00f5ba21ef4cf209ba6f8261f7a1
Abeer Kh Karam <abeerkhalid571@gmail.com>
2020-06-18 16:15:32 +0300

    Sequelize setup  (#19)
    * setup sequelize
    
    * express boilerplate
    
    * Create database
    
    * Merge branch 'master' of https://github.com/GSG-G8/bills-sys into sequelize-setup
    
    * added eslint no warnings
    
    * change the foreignKey to the file whices belong to
    
    * Correct some spelling errors
    
    * Edit name file
    
    * commented sequelize.sync
    
    Co-authored-by: Imad Shatali <amoodaa@gmail.com>

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const compression = require('compression');
+const router = require('./router');
+
+const app = express();
+
+app.use(compression());
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.json());
+app.use('/api/v1/', router);
+module.exports = app;
---
+++ server/src/models/User.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./connection');
+
+const User = sequelize.define('users', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  first_name: { type: DataTypes.STRING, allowNull: false },
+  last_name: { type: DataTypes.STRING },
+  password: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false },
+  is_married: { type: DataTypes.STRING, allowNull: false },
+  mobile_num: { type: DataTypes.STRING, allowNull: false },
+  family_count: { type: DataTypes.INTEGER, allowNull: false },
+});
+module.exports = User;
