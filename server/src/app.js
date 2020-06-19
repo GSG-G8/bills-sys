4b83e46cc8c451ff66342b6e9ecec3bc5ae3d50c
rehabas <reremo45@gmail.com>
2020-06-19 21:19:24 +0300

    install cookie parser relates #11

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const router = require('./router');
 
 const app = express();
@@ -9,5 +10,8 @@ app.disable('x-powered-by');
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/', router);
 module.exports = app;
