df09a7cf90f28238e65d21962f368f832cb7795c
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-06-21 17:02:45 +0300

    update the connection to fit the deifferent enviroments

--- server/src/models/connection.js
+++ server/src/models/connection.js
@@ -1,7 +1,18 @@
 const { Sequelize } = require('sequelize');
 require('env2')('./.env');
 
-const dbUrl = process.env.DATABASE_URL;
+let dbUrl;
+
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = process.env.DATABASE_TEST_URL;
+} else if (process.env.NODE_ENV === 'production') {
+  dbUrl = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === 'development') {
+  dbUrl = process.env.DATABASE_DEV_URL;
+} else {
+  throw new Error('No Database URL!!!');
+}
+
 const sequelize = new Sequelize(dbUrl);
 
 module.exports = sequelize;
