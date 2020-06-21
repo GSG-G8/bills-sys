f1cee89679ed92e92d73289becc30760882a90f7
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-21 15:09:49 +0300

    write tests for getAllBillTypes query

--- server/__tests__/queries/types.test.js
+++ server/__tests__/queries/types.test.js
@@ -1,6 +1,17 @@
-describe('trivial test', () => {
-  it('expect 1 + 1 to be 2', () => {
-    expect.assertions(1);
-    expect(1 + 1).toBe(2);
+const { sequelize } = require('../../src/models/connection');
+const getAllBillTypes = require('../../src/queries/getAllBillTypes');
+
+describe('test queries related to bill types', () => {
+  afterAll(() => sequelize.close());
+  describe('test getAllBillTypes', () => {
+    it('return all bill types', async () => {
+      expect.assertions(5);
+      const billTypes = await getAllBillTypes().map((e) => e.get('name'));
+      expect(billTypes).toHaveLength(4);
+      expect(billTypes).toContain('water');
+      expect(billTypes).toContain('electricity');
+      expect(billTypes).toContain('internet');
+      expect(billTypes).toContain('mobile');
+    });
   });
 });
