d67cb89c7ac81200d9f21b48866ca4c42dd6e22e
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-21 15:10:55 +0300

    write tests for getTypes contorller

--- server/__tests__/routes/types.test.js
+++ server/__tests__/routes/types.test.js
@@ -1,6 +1,17 @@
-describe('trivial test', () => {
-  it('expect 1 + 1 to be 2', () => {
-    expect.assertions(1);
-    expect(1 + 1).toBe(2);
+const request = require('supertest');
+const app = require('../../src/app');
+const { sequelize } = require('../../src/models/connection');
+
+describe('test requests related to bill types', () => {
+  afterAll(() => sequelize.close());
+  describe('test get request to /types', () => {
+    it('should return all bill types with status of 200', async () => {
+      expect.assertions(1);
+      const response = await request(app)
+        .get('/api/v1/types')
+        .set('Accept', 'application/json')
+        .expect(200);
+      expect(response.body).toHaveLength(4);
+    });
   });
 });
