aa1f327a5661757745130717d502d0bc109be5b5
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-06-21 09:49:59 +0300

    update the database URL name to fit the Heroku convention

--- server/src/models/connection.js
+++ server/src/models/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('env2')('./.env');
 
-const sequelize = new Sequelize(process.env.DB_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
