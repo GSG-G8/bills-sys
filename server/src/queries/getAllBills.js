0eef9b42f07114c549164a9bfb7e89765e6712a1
AbeerKaram <abeerkhalid571@gmail.com>
2020-06-22 18:20:52 +0300

    edit quiery

--- server/src/queries/getAllBills.js
+++ server/src/queries/getAllBills.js
@@ -2,7 +2,15 @@ const { Bill } = require('../models');
 
 const getAllBills = () =>
   Bill.findAll({
-    attributes: ['user_id'],
+    attributes: [
+      'id',
+      'billing_year',
+      'billing_month',
+      'amount',
+      'user_id',
+      'type_id',
+    ],
+    where: { user_id: 2 },
   });
 
 module.exports = getAllBills;
