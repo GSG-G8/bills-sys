944b4ed408d63fa6f8cad935e28d61450c9ca2de
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-06-22 06:52:54 +0300

    validate login schema and throw error if there is an error

--- server/src/controller/validation/loginValidation.js
+++ server/src/controller/validation/loginValidation.js
@@ -1,17 +1,26 @@
 const Joi = require('@hapi/joi');
+const Boom = require('@hapi/boom');
 
-const schema = Joi.object({
-  email: Joi.string().email().required().messages({
-    'string.empty': `email is a required field`,
-  }),
-  password: Joi.string()
-    .regex(/[A-z0-9]{6,}/)
-    .required()
-    .messages({
-      'string.empty': `Password is a required field`,
-      'string.pattern.base':
-        'Your password has to be at least six characters or numbers',
+exports.loginValidation = async (req, res, next) => {
+  const { email, password } = req.body;
+  const schema = Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.empty': `email is a required field`,
     }),
-});
-
-module.exports = schema;
+    password: Joi.string()
+      .regex(/[A-z0-9]{6,}/)
+      .required()
+      .messages({
+        'string.empty': `Password is a required field`,
+        'string.pattern.base':
+          'Your password has to be at least six characters or numbers',
+      }),
+  });
+  try {
+    await schema.validate({ email, password }, { abortEarly: false });
+  } catch (error) {
+    if (error === 'ValidationError')
+      throw Boom.badRequest(error.details.map((e) => e.message).join('\n'));
+    next(error);
+  }
+};
