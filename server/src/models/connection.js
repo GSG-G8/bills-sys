1c94a522c9630235d476e69b7d6a560bfc2d1a03
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-06-22 12:08:37 +0300

    delete the commentend test

--- server/src/models/connection.js
+++ server/src/models/connection.js
@@ -3,14 +3,10 @@ require('env2')('./.env');
 
 let dbUrl;
 
-if (process.env.NODE_ENV === 'test') {
+if (dbUrl === 'test') {
   dbUrl = process.env.DATABASE_TEST_URL;
-} else if (process.env.NODE_ENV === 'production') {
-  dbUrl = process.env.DATABASE_URL;
-} else if (process.env.NODE_ENV === 'development') {
-  dbUrl = process.env.DATABASE_DEV_URL;
 } else {
-  throw new Error('No Database URL!!!');
+  dbUrl = process.env.DATABASE_URL;
 }
 
 const sequelize = new Sequelize(dbUrl);
