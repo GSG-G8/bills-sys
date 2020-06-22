53e6a4138ce4ba21b9d80c9195cffd3976f900cf
MohammedIrheem <56350350+MohammedIrheem@users.noreply.github.com>
2020-06-22 20:06:18 +0300

    10 insert script (#20)
    * add the dummy data and insert it in the database
    
    * Insert the data to the database tables
    - delete test code from the connection file
    - export the models directly without using distraction
    - inside the build file we create tables(Users, Bills, Type) and then we collect all the return promises into one promise
    
    * update the database URL name to fit the Heroku convention
    
    * add script to build the database
    
    * delete unused script from the package.json
    
    * update the connection to fit the deifferent enviroments
    
    * delete the commentend test

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,25 +1,3 @@
-const Bill = require('./Bill');
-const Type = require('./Type');
-const User = require('./User');
-// const { sequelize } = require('./connection');
-
-// only enable when changing database, then run the dbBuild script to get back the fake data
-// sequelize.sync({ force: true });
-// Type.bulkCreate([{ name: 'water' }]);
-
-// User.bulkCreate([
-//   {
-//     first_name: 'Abeer',
-//     last_name: 'Karam',
-//     password: 'ak',
-//     email: 'abeer@gmail.com',
-//     is_married: true,
-//     mobile_num: '5576789896',
-//     family_count: 3,
-//   },
-// ]);
-
-// Bill.bulkCreate([
-//   { billing_year: 2020, billing_month: 3, amount: 7, user_id: 1, type_id: 1 },
-// ]);
-module.exports = { Bill, Type, User };
+exports.Bill = require('./Bill');
+exports.Type = require('./Type');
+exports.User = require('./User');
