070118473facdecd9d1a09cdc989c6d279c1fe33
rehabas <reremo45@gmail.com>
2020-06-23 14:33:07 +0300

    edit tests relates #14

--- server/__tests__/routes/bills.test.js
+++ server/__tests__/routes/bills.test.js
@@ -1,6 +1,15 @@
-describe('trivial test', () => {
-  it('expect 1 + 1 to be 2', () => {
+const request = require('supertest');
+const app = require('../../src/app');
+const sequelize = require('../../src/models/connection');
+
+describe('get stats', () => {
+  afterAll(() => sequelize.close());
+  it('route /bills/1/stats?typeId=1&billingMonth=3&billingYear=2020', async () => {
     expect.assertions(1);
-    expect(1 + 1).toBe(2);
+    const result = await request(app)
+      .get('/api/v1/bills/1/stats?typeId=1&billingMonth=3&billingYear=2020')
+      .set('Accept', 'application/json')
+      .expect(200);
+    expect(result.body).not.toHaveLength(0);
   });
 });
