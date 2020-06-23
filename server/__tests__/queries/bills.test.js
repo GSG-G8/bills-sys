2c9a7a944f0d0a1c0fc240d1c1e4c84d4560eec3
AbeerKaram <abeerkhalid571@gmail.com>
2020-06-23 10:11:03 +0300

    Test queiry bills

--- server/__tests__/queries/bills.test.js
+++ server/__tests__/queries/bills.test.js
@@ -1,6 +1,46 @@
-describe('trivial test', () => {
-  it('expect 1 + 1 to be 2', () => {
-    expect.assertions(1);
-    expect(1 + 1).toBe(2);
+const sequelize = require('../../src/models/connection');
+const getAllBills = require('../../src/queries/getAllBills');
+
+describe('test queries related to bills', () => {
+  // eslint-disable-next-line jest/no-hooks
+  afterAll(() => sequelize.close());
+  describe('test getAllBills', () => {
+    it('return all bills', async () => {
+      expect.assertions(5);
+      const bills = await getAllBills().map((e) => e.get());
+      expect(bills).toHaveLength(48);
+      expect(bills).toContainEqual({
+        id: 58,
+        billing_year: 2020,
+        billing_month: 3,
+        amount: 88,
+        user_id: 2,
+        type_id: 2,
+      });
+      expect(bills).toContainEqual({
+        id: 95,
+        billing_year: 2020,
+        billing_month: 12,
+        amount: 177,
+        user_id: 2,
+        type_id: 3,
+      });
+      expect(bills).toContainEqual({
+        id: 88,
+        billing_year: 2020,
+        billing_month: 10,
+        amount: 44,
+        user_id: 2,
+        type_id: 4,
+      });
+      expect(bills).toContainEqual({
+        id: 89,
+        billing_year: 2020,
+        billing_month: 11,
+        amount: 77,
+        user_id: 2,
+        type_id: 1,
+      });
+    });
   });
 });
