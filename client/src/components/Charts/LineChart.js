68666012237021d5ec506ff121ec1699c43ed00f
Imad Shatali <amoodaa@gmail.com>
2020-06-24 22:56:24 +0300

    components files and exports for each issue

---
+++ client/src/components/Charts/LineChart.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LineChart = () => <h1>LineChart</h1>;
+
+LineChart.propTypes = {};
+
+export default LineChart;
---
+++ client/src/components/ToggleButton.js
@@ -0,0 +1,8 @@
+// to be used for language toggle
+import React from 'react';
+
+const Toggle = () => <h1>Toggle</h1>;
+
+Toggle.propTypes = {};
+
+export default Toggle;
