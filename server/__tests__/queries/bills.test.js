6c7ce425f58ed60e0c553419a0cdad49c54216ef
Imad Shatali <amoodaa@gmail.com>
2020-06-24 09:26:58 +0300

    fixed tests

--- server/__tests__/queries/bills.test.js
+++ server/__tests__/queries/bills.test.js
@@ -7,45 +7,45 @@ describe('test queries related to bills', () => {
 
   describe('test getAllBills', () => {
     it('return all bills', async () => {
-      expect.assertions(1);
+      expect.assertions(5);
       const bills = await getAllBills(2).map((e) => e.get({ plain: true }));
       expect(bills).toHaveLength(48);
-      // expect(bills).toContainEqual({
-      //   amount: 97,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 49,
-      //   type: { name: 'water' },
-      //   type_id: 1,
-      //   user_id: 2,
-      // });
-      // expect(bills).toContainEqual({
-      //   amount: 223,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 51,
-      //   type: { name: 'electricity' },
-      //   type_id: 2,
-      //   user_id: 2,
-      // });
-      // expect(bills).toContainEqual({
-      //   amount: 113,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 53,
-      //   type: { name: 'internet' },
-      //   type_id: 3,
-      //   user_id: 2,
-      // });
-      // expect(bills).toContainEqual({
-      //   amount: 63,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 52,
-      //   type: { name: 'mobile' },
-      //   type_id: 4,
-      //   user_id: 2,
-      // });
+      expect(bills).toContainEqual({
+        amount: 97,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 49,
+        type: { name: 'water' },
+        type_id: 1,
+        user_id: 2,
+      });
+      expect(bills).toContainEqual({
+        amount: 223,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 50,
+        type: { name: 'electricity' },
+        type_id: 2,
+        user_id: 2,
+      });
+      expect(bills).toContainEqual({
+        amount: 113,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 51,
+        type: { name: 'internet' },
+        type_id: 3,
+        user_id: 2,
+      });
+      expect(bills).toContainEqual({
+        amount: 63,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 52,
+        type: { name: 'mobile' },
+        type_id: 4,
+        user_id: 2,
+      });
     });
   });
 
--- server/__tests__/routes/bills.test.js
+++ server/__tests__/routes/bills.test.js
@@ -6,7 +6,7 @@ describe('test requests related to bills', () => {
   afterAll(() => sequelize.close());
   describe('test get request to /bills/me', () => {
     it('should return all bills with status of 200', async () => {
-      expect.assertions(1);
+      expect.assertions(5);
       const response = await request(app)
         .get('/api/v1/bills/me')
         .set('Accept', 'application/json')
@@ -15,42 +15,42 @@ describe('test requests related to bills', () => {
         ])
         .expect(200);
       expect(response.body).toHaveLength(48);
-      // expect(response.body).toContainEqual({
-      //   amount: 97,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 49,
-      //   type: { name: 'water' },
-      //   type_id: 1,
-      //   user_id: 2,
-      // });
-      // expect(response.body).toContainEqual({
-      //   amount: 223,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 51,
-      //   type: { name: 'electricity' },
-      //   type_id: 2,
-      //   user_id: 2,
-      // });
-      // expect(response.body).toContainEqual({
-      //   amount: 113,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 53,
-      //   type: { name: 'internet' },
-      //   type_id: 3,
-      //   user_id: 2,
-      // });
-      // expect(response.body).toContainEqual({
-      //   amount: 63,
-      //   billing_month: 1,
-      //   billing_year: 2020,
-      //   id: 52,
-      //   type: { name: 'mobile' },
-      //   type_id: 4,
-      //   user_id: 2,
-      // });
+      expect(response.body).toContainEqual({
+        amount: 97,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 49,
+        type: { name: 'water' },
+        type_id: 1,
+        user_id: 2,
+      });
+      expect(response.body).toContainEqual({
+        amount: 223,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 50,
+        type: { name: 'electricity' },
+        type_id: 2,
+        user_id: 2,
+      });
+      expect(response.body).toContainEqual({
+        amount: 113,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 51,
+        type: { name: 'internet' },
+        type_id: 3,
+        user_id: 2,
+      });
+      expect(response.body).toContainEqual({
+        amount: 63,
+        billing_month: 1,
+        billing_year: 2020,
+        id: 52,
+        type: { name: 'mobile' },
+        type_id: 4,
+        user_id: 2,
+      });
     });
   });
 
