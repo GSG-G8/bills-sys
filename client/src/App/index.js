d3a4aca02da61a8619368b513ecbed0361242599
Imad Shatali <amoodaa@gmail.com>
2020-06-24 16:48:43 +0300

    Prepared react router

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,3 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Layout } from '../components';
+import { Current, Home, Login, PastBills, Profile } from '../pages';
 
-export default () => <h1 className="">Hii from App</h1>;
+const App = () => {
+  const [logged] = useState(() => false);
+  return (
+    <div>
+      <Router>
+        <Layout>
+          <Switch>
+            {!logged && (
+              <Route>
+                <Login />
+              </Route>
+            )}
+            <Route path="/current">
+              <Current />
+            </Route>
+            <Route path="(/|/home)">
+              <Home />
+            </Route>
+            <Route path="/past-bills">
+              <PastBills />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
