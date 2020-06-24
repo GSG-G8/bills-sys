ce3dccc3577c9e4fe8e4a4cc4be77b6ba1c074ae
Imad Shatali <amoodaa@gmail.com>
2020-06-24 09:54:16 +0300

    removed urlencoded

--- server/src/app.js
+++ server/src/app.js
@@ -14,9 +14,8 @@ app.set('port', process.env.PORT || 5000);
 // eslint-disable-next-line import/no-extraneous-dependencies, global-require
 if (process.env.NODE_ENV === 'development') app.use(require('morgan')('dev'));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+const middlewares = [express.json(), cookieParser()];
+app.use(middlewares);
 
 app.use('/api/v1/', router);
 module.exports = app;
