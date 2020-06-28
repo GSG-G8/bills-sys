d11e09be9cfde7ce1021625043040462d31792fe
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-28 11:17:43 +0300

    create nav bar component

--- client/src/components/Layout/NavBar.js
+++ client/src/components/Layout/NavBar.js
@@ -1,3 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default () => <h1> NavBar </h1>;
+import { ReactComponent as Home } from './Home.svg';
+import { ReactComponent as PastBills } from './past bills.svg';
+import { ReactComponent as Profile } from './profile.svg';
+
+export default () => {
+  const history = useHistory();
+  return (
+    <nav className="flex w-full fixed bottom-0 bg-mich-white">
+      <button
+        type="button"
+        className="w-1/3 p-1 flex flex-col items-center text-primary-lighter border-primary-lighter border-t-2 border-r-2 cursor-pointer focus:outline-none"
+        onClick={() => history.push('./home')}
+      >
+        <Home />
+        Home
+      </button>
+      <button
+        type="button"
+        className="w-1/3 p-1 flex flex-col items-center text-primary-lighter border-primary-lighter border-t-2 cursor-pointer focus:outline-none"
+        onClick={() => history.push('./past-bills')}
+      >
+        <PastBills />
+        Past Bills
+      </button>
+      <button
+        type="button"
+        className="w-1/3 p-1 flex flex-col items-center text-primary-lighter border-primary-lighter border-t-2 border-l-2 cursor-pointer focus:outline-none"
+        onClick={() => history.push('./profile')}
+      >
+        <Profile />
+        My profile
+      </button>
+    </nav>
+  );
+};
