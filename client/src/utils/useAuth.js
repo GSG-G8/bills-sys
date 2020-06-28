50bb3da7f10499bc0f406cc44ec192a6492fa47f
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-28 09:31:06 +0300

    created useAuth custom hook to check if the user logged in or not

---
+++ client/src/utils/useAuth.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useAuth = () => {
+  const [logged, setLogged] = useState('loading');
+  const [userId, setUserId] = useState('');
+
+  useEffect(() => {
+    fetch('/api/v1/auth')
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.statusCode === 200) {
+          setLogged(true);
+          setUserId(result.user.id);
+        } else {
+          setLogged(false);
+          setUserId('');
+        }
+      })
+      .catch(() => {
+        setLogged(false);
+        setUserId('');
+      });
+  }, []);
+
+  return [logged, setLogged, userId, setUserId];
+};
+
+export default useAuth;
