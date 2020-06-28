8be9efdca745e81133043918461cb9a9b53d264c
AbeerKaram <abeerkhalid571@gmail.com>
2020-06-28 13:16:16 +0300

    Toggle component to show Tips

--- client/src/components/ToggleContainer.js
+++ client/src/components/ToggleContainer.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import toggle from '../assets/up-arrow-svgrepo-com.svg';
+import Tips from './Tips';
 
-const ToggleContainer = () => <h1>ToggleContainer</h1>;
+const ToggleContainer = () => {
+  const [showTips, setShowTips] = useState(false);
+  const toggleTips = () => setShowTips(!showTips);
+
+  return (
+    <div className="flex justify-between p-10">
+      <h1 className="text-3xl">Tips</h1>
+      {showTips && <Tips />}
+      <button type="button" onClick={toggleTips}>
+        <img
+          className={`w-10 ${showTips ? 'transform rotate-180' : ''}`}
+          src={toggle}
+          alt="toggleContainer"
+        />
+      </button>
+    </div>
+  );
+};
 
 ToggleContainer.propTypes = {};
 
