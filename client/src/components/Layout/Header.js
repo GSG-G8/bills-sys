ff31b63a7eb9329b3bcc144913b71177a8c5286f
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-06-30 09:59:02 +0300

    add toggle btns to the header

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import LanguageToggle from '../ToggleButton';
 
-const Header = () => <h1> Header </h1>;
+const Header = () => {
+  const { t } = useTranslation();
 
+  return (
+    <div className="bg-green-600 flex justify-between p-6">
+      <h1>{t('logo')}</h1>
+      <LanguageToggle />
+    </div>
+  );
+};
 export default Header;
--- client/src/components/ToggleButton.js
+++ client/src/components/ToggleButton.js
@@ -4,27 +4,13 @@ import { useTranslation } from 'react-i18next';
 const Toggle = () => {
   const { i18n } = useTranslation();
 
+  const onLanguageSelect = (e) => i18n.changeLanguage(e.target.value);
+
   return (
-    <div>
-      <button
-        className="bg-transparent hover:bg-green-600 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        type="submit"
-        onClick={() => {
-          i18n.changeLanguage('ar');
-        }}
-      >
-        Ar
-      </button>
-      <button
-        className="bg-transparent hover:bg-green-600 text-blue-500 font-semibold hover:text-white py-2 px-4 hover : border-green-600 rounded"
-        type="submit"
-        onClick={() => {
-          i18n.changeLanguage('en');
-        }}
-      >
-        EN
-      </button>
-    </div>
+    <select onChange={onLanguageSelect}>
+      <option value="ar">ar</option>
+      <option value="en">en</option>
+    </select>
   );
 };
 
