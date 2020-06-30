c7ad5c1c9630f6e8966240e5c88594352421a57f
AbeerKaram <abeerkhalid571@gmail.com>
2020-06-30 16:32:12 +0300

    Merge branch 'master' into 38-LineChart

--- client/src/pages/PastBills.js
+++ client/src/pages/PastBills.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { LineChart } from '../components';
 
-const PastBills = () => <h1> PastBills </h1>;
+const bills = [120, 60, 40, 50, 80, 25, 55, 100, 120, 20, 30, 70];
+
+const PastBills = () => <LineChart bills={bills} />;
 
 export default PastBills;
