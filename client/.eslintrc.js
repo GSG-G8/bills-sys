1c70822b8721578c8ee11edec83d409935477ca7
Imad Shatali <imad.shatali@gmail.com>
2020-06-30 11:03:27 +0300

    Added rule to prevent using promises and default to async await added axios in front

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,37 +1,38 @@
 module.exports = {
-  extends: ["airbnb","airbnb/hooks", "prettier", "prettier/react"],
+  extends: ["airbnb", "airbnb/hooks", "prettier", "prettier/react"],
   // babel-eslint parser is used to support experimental features not supported in ESLint itself yet
   parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 11,
     ecmaFeatures: {
-      impliedStrict: true //enable global strict mode (if ecmaVersion is 5 or greater)
-    }
+      impliedStrict: true, //enable global strict mode (if ecmaVersion is 5 or greater)
+    },
   },
   root: true,
   env: {
     browser: true,
     node: true,
-    jest: true
+    jest: true,
   },
   rules: {
+    "promise/prefer-await-to-then": "error",
     "arrow-body-style": ["error", "as-needed"],
     "react/state-in-constructor": 0,
     //  allow .js extensions for JSX.
     "react/jsx-filename-extension": [
       1,
       {
-        extensions: [".js", ".jsx"]
-      }
+        extensions: [".js", ".jsx"],
+      },
     ],
     // configure the prettier plugin
     "prettier/prettier": [
       "error",
       {
         trailingComma: "es5",
-        singleQuote: true
-      }
-    ]
+        singleQuote: true,
+      },
+    ],
   },
-  plugins: ["react", "prettier"]
+  plugins: ["react", "prettier", "promise"],
 };
