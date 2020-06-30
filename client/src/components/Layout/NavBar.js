a161ef4d15a0fc5c505a1d0681e89a5ae6b8849d
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-30 13:59:35 +0300

    remove text from icons and fix its padding for larger screens

--- client/src/components/Layout/NavBar.js
+++ client/src/components/Layout/NavBar.js
@@ -11,27 +11,24 @@ const NavBar = () => {
     <nav className="flex w-full fixed bottom-0 right-0 lg:block lg:top-center lg:w-24 lg:right-auto">
       <button
         type="button"
-        className="w-1/3 p-1 flex flex-col items-center bg-mich-white text-primary-lighter border-primary-lighter border-t-2 border-r-2 cursor-pointer focus:outline-none lg:w-full lg:pt-6 lg:pb-6"
+        className="w-1/3 pr-1 pl-1 pt-2 pb-2 flex flex-col items-center bg-mich-white text-primary-lighter border-primary-lighter border-t-2 border-r-2 cursor-pointer focus:outline-none lg:pl-0 lg:pr-0 lg:pt-4 lg:pb-4 lg:w-1/2"
         onClick={() => history.push('./home')}
       >
         <Home />
-        Home
       </button>
       <button
         type="button"
-        className="w-1/3 p-1 flex flex-col items-center bg-mich-white text-primary-lighter border-primary-lighter border-t-2 cursor-pointer focus:outline-none lg:border-b-2 lg:border-r-2 lg:w-full lg:pt-6 lg:pb-6"
+        className="w-1/3 pr-1 pl-1 pt-2 pb-2 flex flex-col items-center bg-mich-white text-primary-lighter border-primary-lighter border-t-2 cursor-pointer focus:outline-none lg:border-b-2 lg:border-r-2  lg:pl-0 lg:pr-0 lg:pt-4 lg:pb-4 lg:w-1/2"
         onClick={() => history.push('./past-bills')}
       >
         <PastBills />
-        Past Bills
       </button>
       <button
         type="button"
-        className="w-1/3 p-1 flex flex-col items-center bg-mich-white text-primary-lighter border-primary-lighter border-t-2 border-l-2 cursor-pointer focus:outline-none lg:border-t-0 lg:border-b-2 lg:border-l-0 lg:border-r-2 lg:w-full lg:pt-6 lg:pb-6"
+        className="w-1/3 pr-1 pl-1 pt-2 pb-2 flex flex-col items-center bg-mich-white text-primary-lighter border-primary-lighter border-t-2 border-l-2 cursor-pointer focus:outline-none lg:border-t-0 lg:border-b-2 lg:border-l-0 lg:border-r-2 lg:pl-0 lg:pr-0 lg:pt-4 lg:pb-4  lg:w-1/2"
         onClick={() => history.push('./profile')}
       >
         <Profile />
-        My profile
       </button>
     </nav>
   );
