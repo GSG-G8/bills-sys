cac119f7c590b5f0f69e615ab25de06ab4a7c038
MohammedIrheem <56350350+MohammedIrheem@users.noreply.github.com>
2020-06-30 14:02:53 +0300

    language toggle (#52)
    * add language files
    
    * setup localization configration using i18n
    
    * add languages files
    
    * localization configration
    
    * create toggle component
    
    * add toggle btns to the header
    
    * change toggle button file
    wrap the app by suspense and delete it from the index file
    
    * delete unused package

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from '../components';
 import { Current, Home, Login, PastBills, Profile } from '../pages';
@@ -7,29 +7,31 @@ const App = () => {
   const [logged] = useState(() => false);
   return (
     <div>
-      <Router>
-        <Layout>
-          <Switch>
-            {!logged && (
-              <Route>
-                <Login />
+      <Suspense fallback={<div />}>
+        <Router>
+          <Layout>
+            <Switch>
+              {!logged && (
+                <Route>
+                  <Login />
+                </Route>
+              )}
+              <Route path="/current">
+                <Current />
               </Route>
-            )}
-            <Route path="/current">
-              <Current />
-            </Route>
-            <Route path="(/|/home)">
-              <Home />
-            </Route>
-            <Route path="/past-bills">
-              <PastBills />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-          </Switch>
-        </Layout>
-      </Router>
+              <Route path="(/|/home)">
+                <Home />
+              </Route>
+              <Route path="/past-bills">
+                <PastBills />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+            </Switch>
+          </Layout>
+        </Router>
+      </Suspense>
     </div>
   );
 };
---
+++ client/src/i18n.js
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import XHR from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}.json',
+      allowMultiLoading: true,
+    },
+    react: {
+      useSuspense: true,
+    },
+  });
+export default i18n;
