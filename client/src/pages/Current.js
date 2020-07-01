509e5f21f67fa881d70e2953991485d70493a49e
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-07-01 15:29:40 +0300

    delete props prototypes

--- client/src/pages/Current.js
+++ client/src/pages/Current.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import propTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 import ToggleContainer from '../components/ToggleContainer';
@@ -20,8 +19,4 @@ const Current = () => {
   );
 };
 
-Current.propTypes = {
-  billType: propTypes.oneOf(['water', 'electricity', 'mobile', 'internet'])
-    .isRequired,
-};
 export default Current;
