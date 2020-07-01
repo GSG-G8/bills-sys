f0ad6054e40580247e1c7d6abaf6c61c9b15186c
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-07-01 15:08:02 +0300

    style the tips and toggle component

--- client/src/pages/Current.js
+++ client/src/pages/Current.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+
 import ToggleContainer from '../components/ToggleContainer';
 import Tips from '../components/Tips';
 import Table from '../components/Table';
 
-const Current = ({ billType }) => (
-  <>
-    <ToggleContainer title="Tips">
-      <Tips billType={billType} />
-    </ToggleContainer>
-    <ToggleContainer title="Compare Table">
-      <Table />
-    </ToggleContainer>
-  </>
-);
+const Current = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <ToggleContainer title={t('tipsToggleTitle')}>
+        <Tips billType="electricity" />
+      </ToggleContainer>
+      <ToggleContainer title={t('compareTableTitle')}>
+        <Table />
+      </ToggleContainer>
+    </>
+  );
+};
 
 Current.propTypes = {
   billType: propTypes.oneOf(['water', 'electricity', 'mobile', 'internet'])
