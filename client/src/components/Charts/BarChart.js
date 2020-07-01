2ea0d2c642b14b31fd8f81c20801e1dea4ae2a96
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-07-01 15:54:35 +0300

    creat bar chart component

--- client/src/components/Charts/BarChart.js
+++ client/src/components/Charts/BarChart.js
@@ -2,57 +2,18 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { Bar } from 'react-chartjs-2';
 
-const sortValues = (arr) => {
-  const values = [...arr];
-  values.sort((a, b) => a - b);
-  return values;
-};
-
-// this will trim the outliers values from both the start and the end of the data array
-const trimmer = (bound, data) => {
-  const values = [...data];
-  for (let i = 0; i < Math.floor(bound); i += 1) {
-    values.pop();
-    values.shift();
-  }
-  return values;
-};
-
-/* 
-this will find the outliers values in the dataset and decide the count to trim from both sides 
-and find the trimmed mean
-*/
-const trimmedMean = (data) => {
-  const values = sortValues([...data]);
-  const valuesCount = values.length;
-  // this is the trimming proportion, it mean that we want to trim 10% of the dataset
-  const proportionTrimmed = 0.1;
-  // decides how many values should we trim
-  const trimCount = valuesCount * proportionTrimmed;
-  // decides how many values will remain after trimming
-  const remainingValuesCount = valuesCount - 2 * trimCount;
-  const trimmedValues = trimmer(trimCount, values);
-  // if the number to trim is not an integer this will handle the issue
-  if (!Number.isInteger(trimCount)) {
-    trimmedValues[0] *= Math.abs(1 - trimCount);
-    trimmedValues[trimmedValues.length - 1] *= Math.abs(1 - trimCount);
-  }
-  const valuesSum = trimmedValues.reduce((a, b) => a + b);
-  return valuesSum / remainingValuesCount;
-};
-
-const BarChart = ({ data }) => (
-  <div>
+const BarChart = ({ centers, frequencies }) => (
+  <div className="p-4 m-auto md:w-2/3 lg:w-1/2">
     <Bar
       data={{
-        labels: [25, 50, 75, 100],
+        labels: centers,
         datasets: [
           {
             label: 'Frequency',
-            data: [86, 67, 91, 70],
-            backgroundColor: ['red', 'green', 'blue', 'black'],
-            borderColor: 'blue',
-            borderWidth: 5,
+            data: frequencies,
+            backgroundColor: 'green',
+            barThickness: 'flex',
+            barPercentage: 1,
           },
         ],
       }}
@@ -61,14 +22,42 @@ const BarChart = ({ data }) => (
           display: true,
           text: 'Custom Chart Title',
         },
+        responsive: true,
+        maintainAspectRatio: true,
+        aspectRatio: 1,
+        plugins: {
+          datalabels: {
+            font: {
+              size: 0,
+            },
+          },
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                offsetGridLines: true,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: Math.max(...frequencies) + 10,
+                stepSize: 10,
+              },
+            },
+          ],
+        },
       }}
     />
-    <p>the mean is {trimmedMean(data)}</p>
   </div>
 );
 
 BarChart.propTypes = {
-  data: propTypes.arrayOf(propTypes.number).isRequired,
+  centers: propTypes.arrayOf(propTypes.number).isRequired,
+  frequencies: propTypes.arrayOf(propTypes.number).isRequired,
 };
 
 export default BarChart;
