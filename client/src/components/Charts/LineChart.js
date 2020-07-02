9112f99c187cf07705af67b3862079f74f7c8641
Abeer Kh Karam <abeerkhalid571@gmail.com>
2020-07-02 09:20:36 +0300

    Line Chart Component (#57)
    * fix rule promise in eslint
    
    * Merge branch 'master' into 38-LineChart
    
    * Extract data out of the function

--- client/src/components/Charts/LineChart.js
+++ client/src/components/Charts/LineChart.js
@@ -1,7 +1,77 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { Line } from 'react-chartjs-2';
 
-const LineChart = () => <h1>LineChart</h1>;
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const LineChart = ({ bills }) => {
+  const data = {
+    labels: months,
+    datasets: [
+      {
+        label: 'Past Bills $',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 2,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: bills,
+      },
+    ],
+  };
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            max: Math.max(...bills) + 20,
+            stepSize: 20,
+          },
+        },
+      ],
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      datalabels: {
+        font: {
+          size: 0,
+        },
+      },
+    },
+  };
 
-LineChart.propTypes = {};
+  return (
+    <div>
+      <h2>Line Chart</h2>
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+LineChart.propTypes = {
+  bills: propTypes.arrayOf(propTypes.number).isRequired,
+};
 
 export default LineChart;
