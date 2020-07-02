7d5e86b44ed235594802d344df018a988fe22b79
rehabas <reremo45@gmail.com>
2020-07-02 13:39:08 +0300

    sort getMonthlyBills array relates #30

--- client/src/utils/getMonthlyBills.js
+++ client/src/utils/getMonthlyBills.js
@@ -2,10 +2,10 @@ const getMonthlyBills = (bills) => {
   const allBills = [];
   const month = new Date().getMonth() + 1;
   const dd = bills.filter((bill) => bill.billing_month === month);
-  dd.forEach(({ type: { name }, amount }) => {
-    allBills.push({ name, amount });
+  dd.forEach(({ type_id: id, type: { name }, amount }) => {
+    allBills.push({ id, name, amount });
   });
-  return allBills;
+  return allBills.sort((a, b) => a.id - b.id);
 };
 
 export default getMonthlyBills;
