a4ba2b1a3cd15d2ee4b680c90f13e9ab05abb11c
MohammedIrheemde <mohammedirheem96@gmail.com>
2020-07-05 13:38:39 +0300

    change the camelCase words in the translation files to normal case

--- client/src/pages/Current.js
+++ client/src/pages/Current.js
@@ -31,10 +31,10 @@ const Current = () => {
     <>
       <BarChart centers={centers} frequencies={frequencies} />
       <div className="mx-4 lg:mx-16 lg:mx-8 lg:my-8 md:mx-10 md:mx-5 md:my-5">
-        <ToggleContainer title={t('tipsToggleTitle')}>
+        <ToggleContainer title={t('tips-toggle-title')}>
           <Tips billType={billType} />
         </ToggleContainer>
-        <ToggleContainer title={t('compareTableTitle')}>
+        <ToggleContainer title={t('compare-table-title')}>
           <Table bills={billsOfPageType} />
         </ToggleContainer>
       </div>
