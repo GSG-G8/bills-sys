0f9686c36b6367a0f52b640ca6c63c4ddcd17949
MohammedIrheem <56350350+MohammedIrheem@users.noreply.github.com>
2020-07-05 16:10:42 +0300

    profile page (#61)
    * add user profile image
    
    * query to get user data
    
    * create function to get the signed in user data
    
    * display user data
    
    * add attrubuit to delete user password from the sending data
    
    * check if there is an error return from the server
    
    * alignuser data in the center
    
    * update getProfile query to get just one element
    
    * create logout button

---
+++ client/src/components/Logout.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import propTypes from 'prop-types';
+
+const Logout = ({ setLogged, setUserId }) => {
+  const [error, setError] = useState('');
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    try {
+      await axios.get('api/v1/logout');
+      setUserId('');
+      setLogged(false);
+      history.push('/');
+    } catch (err) {
+      if (err.response) setError(err.response.data.message);
+      else setError('Something went wrong!');
+    }
+  };
+
+  return (
+    <div>
+      <button
+        type="submit"
+        onClick={handleLogout}
+        className="mt-12 mb-4 pl-16 pr-16 pt-2 pb-2 rounded-full w-64 text-mich-white leading-7 font-medium bg-primary transform md:w-1/3 lg:w-1/4 hover:bg-primary-dark focus:bg-primary-dark focus:outline-none focus:scale-105 transition duration-500 ease-in-out"
+      >
+        Logout
+      </button>
+      <h3 className="bg-red">{error}</h3>
+    </div>
+  );
+};
+
+Logout.propTypes = {
+  setLogged: propTypes.func.isRequired,
+  setUserId: propTypes.func.isRequired,
+};
+
+export default Logout;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import propTypes from 'prop-types';
 
-const Profile = () => <h1> Profile </h1>;
+import { ReactComponent as User } from '../assets/user.svg';
+import { Loader } from '../components';
+import Logout from '../components/Logout';
+
+const Profile = ({ setLogged, setUserId }) => {
+  const [profile, setProfile] = useState();
+  const [error, setError] = useState('');
+  const getProfile = async () => {
+    try {
+      const { data } = await axios.get('api/v1/profile');
+      setProfile(data);
+    } catch (err) {
+      if (err.response) setError(err.response.data.message);
+      else setError('Something went error');
+    }
+  };
+  useEffect(() => {
+    getProfile();
+  }, []);
+  if (error) return <p>{error}</p>;
+  if (!profile) return <Loader />;
+  return (
+    <div className="m-auto text-center">
+      <User className="text-center m-auto mt-10" />
+      <div className="m-4 font-bold">
+        {profile.first_name} {profile.last_name}
+      </div>
+      <div className="m-6">
+        <h1>Mobile: {profile.mobile_num}</h1>
+        <h1>Email: {profile.email}</h1>
+        <h1>Number of people in my house: {profile.family_count} persons</h1>
+      </div>
+      <Logout setLogged={setLogged} setUserId={setUserId} />
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  setLogged: propTypes.func.isRequired,
+  setUserId: propTypes.func.isRequired,
+};
 
 export default Profile;
